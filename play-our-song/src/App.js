7fcaf94796f68b124a7aef235aa8365935d7c52c
Kylie Chesner <kyliec362@gmail.com>
2019-10-15 20:49:20 -0500

    top bar title wrap fix

--- play-our-song/src/App.js
+++ play-our-song/src/App.js
@@ -24,7 +24,6 @@ export const useStyles = makeStyles(theme => ({
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
-    marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(3),
